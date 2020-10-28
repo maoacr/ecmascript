0d7cd0a13fd2ccd557fc5fdeea77d5f366a548c6
Mario A. Crespo Reyes <maoacr@gmail.com>
2020-10-28 18:19:50 -0500

    Padding

--- src/es8/index.js
+++ src/es8/index.js
@@ -15,3 +15,10 @@ console.log(entries.length);
 const values = Object.values(data)
 console.log(values);
 console.log(values.length);
+
+//Padding
+
+const string = 'hello';
+console.log(string.padStart(7, 'hi'));
+console.log(string.padEnd(12, '----'));
+console.log(string.padEnd(12, '----'));
