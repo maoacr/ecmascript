8151af850057ce79761dfb760cf700726491f6e8
Mario A. Crespo Reyes <maoacr@gmail.com>
2020-10-28 18:40:33 -0500

    Async/Await

--- src/es8/index.js
+++ src/es8/index.js
@@ -28,3 +28,32 @@ console.log(string.padEnd(12, '----'));
 const object = {
   name: 'oscar',
 }
+
+
+//Async/Await
+
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    (true) 
+      ? setTimeout(() => resolve('Hello World'),3000)
+      : reject(new Error('Test Error'));
+  })
+}
+
+const helloAsync = async() => {
+  const hello = await helloWorld();
+  console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld();
+    console.log(hello)
+  } catch (error){
+    console.log(error)
+  }
+}
+
+anotherFunction();
