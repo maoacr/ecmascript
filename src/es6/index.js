da475d7dae0323eaefe2052221baf6c1ef880e75
Mario A. Crespo Reyes <maoacr@gmail.com>
2020-10-27 21:11:19 -0500

    modules and generators

--- src/es6/index.js
+++ src/es6/index.js
@@ -123,3 +123,38 @@ helloPromise()
 
 //Clases
 
+class calculator {
+  constructor() {
+    this.valueA = 0;
+    this.valueB = 0;
+  }
+  sum(valueA, valueB) {
+    this.valueA = valueA;
+    this.valueB = valueB;
+    return this.valueA + this.valueB;
+  }
+}
+const calc = new calculator();
+console.log(calc.sum(2, 2));
+
+
+//Módulos
+import myFunction from './module';
+myFunction();
+
+
+//Generators
+
+function* helloWorld() {
+  if (true){
+    yield 'Heloooo !';
+  }
+  if (true) {
+    yield 'world';
+  }
+}
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
