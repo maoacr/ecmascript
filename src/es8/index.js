f4c677b69885e0b572be9338910512842da2d83f
Mario A. Crespo Reyes <maoacr@gmail.com>
2020-10-28 18:24:50 -0500

    Trailing comas

--- src/es8/index.js
+++ src/es8/index.js
@@ -22,3 +22,9 @@ const string = 'hello';
 console.log(string.padStart(7, 'hi'));
 console.log(string.padEnd(12, '----'));
 console.log(string.padEnd(12, '----'));
+
+//Trailing comas 
+
+const object = {
+  name: 'oscar',
+}
