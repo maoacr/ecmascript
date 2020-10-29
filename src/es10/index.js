4b6597bcd14d33ca8c43351fbd61f24f22f3cd82
Mario A. Crespo Reyes <maoacr@gmail.com>
2020-10-28 23:42:09 -0500

    es10

---
+++ src/es10/index.js
@@ -0,0 +1,9 @@
+let array = [1,2,3,[1,2,3,[1,2,3]]];
+console.log(array.flat())
+
+let array2 = [1,2,3,4,5,6];
+console.log(array.flatMap(value => [value, value * 2]))
+
+let = hello = '            hello world';
+console.log(hello);
+console.log(hello.trimStart());
