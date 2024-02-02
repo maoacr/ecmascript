785d67834bf58a3901f35abdb2352ceb2247bd61
Mao <maoacr@gmail.com>
2024-02-02 15:25:15 -0500

    Updating strings files with new Platzi course

---
+++ src/es6/03-strings.js
@@ -0,0 +1,18 @@
+//Strings antes del ES6
+let username = "Mario";
+let lastname = "Crespo";
+let fullName = username + " " + lastname;
+
+//Template literals
+let nombre = `Mario`;
+let apellido = `Crespo Reyes`;
+let nombreCompleto = `${nombre} ${apellido}`;
+
+//Multilinea
+
+let saltoDeLinea = "Esta es mi primera linea \n" + "Y esta es mi segund linea";
+
+let myTexto = ` Mi primera linea
+Mi segunda linea de texto
+
+Mi tercera linea de texto despues de un salto de linea`;
