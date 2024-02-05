cba65c8bf3116e1a5d77144d4c0b62e2f8b14764
Mao <maoacr@gmail.com>
2024-02-05 17:54:37 -0500

    Create rest spread examples file

---
+++ src/es6/05-rest-spread.js
@@ -0,0 +1,11 @@
+// Array destructuring
+
+let fruits = ["Banana", "Apple"];
+let [a, b] = fruits;
+console.log(a, b);
+
+// Object desctructuring
+
+let user = { username: "Mao", age: 30 };
+let { username, age } = user;
+console.log(username, age);
