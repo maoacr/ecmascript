78a805ac644ac220c3b21a26c85734db5e630f2b
Mao <maoacr@gmail.com>
2024-02-05 23:05:02 -0500

    Create object literal enhaced examples

---
+++ src/es6/06-object-literals.js
@@ -0,0 +1,12 @@
+// Enhaced object literal
+
+function newUser(name, lastname, user, age) {
+  return {
+    name,
+    lastname,
+    user,
+    age,
+  };
+}
+
+console.log(newUser("Mayito", "Crespo", "@maoacr", 30));
